4a851cc86033cb84e8668d6b19a59f85fd183f2b
Mark Feeney <mark.feeney@gmail.com>
2017-02-05 22:53:05 -0500

    Get auto-sizing canvas working with no scrollbars
    Harder than it should have been. Border pixels!

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,60 @@
 console.log("app.ts loaded");
 
-function createCanvas(width: number, height: number): HTMLCanvasElement {
+type Margin = {top: number; right: number; bottom: number; left: number};
+
+/** Return a new canvas with given width, height, and margin. */
+function createCanvas(width: number, height: number, margin: Margin, border: string): HTMLCanvasElement {
     const canvas = document.createElement("canvas");
-    canvas.setAttribute("width", width.toString());
-    canvas.setAttribute("height", height.toString());
+    const s = canvas.style;
+    s.width = width + "px";
+    s.height = height + "px";
+    s.marginTop = margin.top + "px";
+    s.marginRight = margin.right + "px";
+    s.marginBottom = margin.bottom + "px";
+    s.marginLeft = margin.left + "px";
+    s.border = border;
+    s.display = "block"; // disables scrollbars
     return canvas;
 }
 
+/** Return the viewport size in pixels: width, height. */
+function viewportSize(): [number, number] {
+    const de = document.documentElement;
+    const width = Math.max(de.clientWidth, window.innerWidth || 0);
+    const height = Math.max(de.clientHeight, window.innerHeight || 0);
+    return [width, height];
+}
+
 function main(): void {
-    const body = document.querySelector("body");
-    if (!body) {
-        throw new Error("no body element in document");
+
+    const container = document.querySelector("#canvas-container");
+    if (!container) {
+        throw new Error("no canvas container element");
     }
 
-    const canvas = createCanvas(600, 400);
-    body.appendChild(canvas);
+    const [viewportW, viewportH] = viewportSize();
+    const margin = {top: 50, right: 50, bottom: 50, left: 50};
+    const borderThickness = 1;
+    const canvasW = viewportW - margin.left - margin.right;
+    const canvasH = viewportH - margin.top - margin.bottom - 2 * borderThickness;
+
+    const canvas = createCanvas(
+        canvasW,
+        canvasH,
+        margin,
+        `${borderThickness}px solid #333`);
+
+    container.appendChild(canvas);
+
+    const context = canvas.getContext("2d");
+    if (!context) {
+        throw new Error("could not get 2d context");
+    }
+    context.beginPath();
+    context.fillStyle = "#b8c7e0";
+    context.fillRect(0, 0, canvasW, canvasH);
+
+
 }
 
 main();
--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, "/dist"),
         compress: true,
-        port: 8000
+        port: 8000,
+        host: "0.0.0.0"
     }
 };
