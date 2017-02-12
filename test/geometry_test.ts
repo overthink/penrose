9eaf52c963493b1c63616cf47ae4787c29caa8e0
Mark Feeney <mark.feeney@gmail.com>
2017-02-11 23:08:01 -0500

    Fancier test output

--- test/geometry_test.ts
+++ test/geometry_test.ts
@@ -3,7 +3,16 @@ import {Vector} from "../src/geometry";
 
 test("Vector properties work", t => {
     const v = new Vector(3, 5);
-    t.equal(v.x, 3);
-    t.equal(v.y, 5);
+    t.equal(v.x, 3, "x property works");
+    t.equal(v.y, 5, "y property works");
     t.end();
+});
+
+test("test that waits around", t => {
+    t.plan(3);
+    t.equal(1, 1, "one is one");
+    setTimeout(() => {
+        t.equal(42, 42, "answer ok");
+    }, 500);
+    t.equal(2, 2, "two is 2.");
 });
